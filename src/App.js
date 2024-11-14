fc32f9e394e909b84bfff49f7b9f91b0b9b216c0
victor-devdesign <victor.carvalho.araujo04@gmail.com>
2024-11-13 23:58:54 -0300

    - Estilização de tela inicial; - Configuração do bootstrap;

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
 
+// Importing Sass with Bootstrap CSS
+import './scss/App.scss';
+
 function App() {
   return <AppRoutes />;
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,21 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome to Jokenpo!</h1>
-            <Link to="/play">
-                <button>Start Game</button>
-            </Link>
+        <div className="container-home">
+            <div className="row">
+                <div className="col-12 d-flex justify-content-center m-2">
+                    <div className="home-logo">
+                        <img src="/img/logo512.png" alt="Logo" />
+                    </div>
+                </div>
+                <div className="col-12 d-flex justify-content-center m-2">
+                    <Link to="/play">
+                        <div className="home-btn">
+                            <img src="/img/btn_start_game.png" alt="Start Button" />
+                        </div>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
